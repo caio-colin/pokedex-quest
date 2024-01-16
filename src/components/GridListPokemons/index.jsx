598d564a54a9b12c78ba85c7b718aa04427baf3a
caio-colin <caiio_c@hotmail.com>
2024-01-16 12:01:23 -0300

    Ajuste na logica de foco do pokemon ao retornar da pagina detalhes

--- src/components/GridListPokemons/index.jsx
+++ src/components/GridListPokemons/index.jsx
@@ -12,20 +12,24 @@ export const GridListPokemons = ({ children, loading, pokemonsList }) => {
   useEffect(() => {
     const timeoutId = setTimeout(() => {
       if (pokemonNameSelected && pokemonClick.current) {
-        // pokemonClick.current.scrollIntoView({
-        //   behavior: "smooth",
-        //   block: "center",
-        // })
+        pokemonClick.current.scrollIntoView({
+          behavior: "smooth",
+          block: "center",
+        })
         // Ao remover pokemonSelected do Storage garante que o Card perca a seleção ao relogar a pagina
         sessionStorage.removeItem("pokemonSelected")
       }
+
+      // O Tempo tem que ser multiplo de 2 segundos(tempo da animacão definido no style) e diminuir o valor pelo tempo do setTimeout principal, no caso ai ta sendo 4 segundos menos 200ms do tempo do timeout principal
+      setTimeout(() => removeFocus(), 3800)
     }, 200)
 
-    return () => {
-      console.log("desmontou")
-      clearTimeout(timeoutId)
-    }
+    return () => clearTimeout(timeoutId)
   }, [loading])
+
+  const removeFocus = () => {
+    pokemonClick.current?.classList.remove("item-selected")
+  }
 
   const handleClick = (pokemonName) => {
     // Guardar o nome do pokemon selecionado na sessionStorage

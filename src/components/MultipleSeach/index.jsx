0d63473b898f218843023081eb00954538e52344
caio-colin <caiio_c@hotmail.com>
2024-01-23 14:40:15 -0300

    Adicionado Drop no MultipleSeach ao scrolar

--- src/components/MultipleSeach/index.jsx
+++ src/components/MultipleSeach/index.jsx
@@ -4,7 +4,6 @@ import { useConditionFiltering } from "../../Hooks/index.jsx"
 import { getPokemons } from "../../services/requestAPI"
 import { SelectFilterTypePokemon } from "../SelectFilterTypePokemon"
 import { StyleContainerMultipleSearch } from "./styled.jsx"
-import { useInView } from "react-intersection-observer"
 
 export const MultipleSeach = ({
   listNames = [],
@@ -14,15 +13,13 @@ export const MultipleSeach = ({
   const nameSession = JSON.parse(sessionStorage.getItem("nameSelectedOnPage"))
   const [nameSelectedOnPage, setNameSelectedOnPage] = useState("")
   const [listPokemonsByType, setListPokemonsByType] = useState([])
+  const [widthParent, setWidthParent] = useState(null)
   const [valueByFather, setValueByFather] = useState("")
+  const [isElementAboveViewport, setIsElementAboveViewport] = useState(false)
   const isMounted = useRef(false)
+  const elementObserved = useRef(null)
   const timeOutId = useRef(null)
   const [progressTime, setProgressTime] = useState(null)
-  const [refView, inView] = useInView({
-    root: null,
-    rootMargin: "0px",
-    threshold: 0.1,
-  })
 
   const resultFiltering = useConditionFiltering(
     nameSelectedOnPage,
@@ -35,7 +32,7 @@ export const MultipleSeach = ({
     const filteredResult = await getPokemons(list)
 
     setListSearchFilter(filteredResult)
-
+    window.scrollTo(0, 0)
     setFiltering(false)
   }
   const removeFocus = () => {
@@ -54,6 +51,7 @@ export const MultipleSeach = ({
       removeFocus()
       setTimeout(() => {
         const handleFalse = () => {
+          window.scrollTo(0, 0)
           setFiltering(false)
           setListSearchFilter(null)
         }
@@ -66,6 +64,33 @@ export const MultipleSeach = ({
       }, 500)
     }, 1000)
   }
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (elementObserved.current) {
+        const rect = elementObserved.current.getBoundingClientRect()
+        const isAboveViewport = rect.bottom < 0
+        setIsElementAboveViewport(isAboveViewport)
+      }
+    }
+
+    const handleResize = () => {
+      if (elementObserved.current) {
+        const widthParentElement = elementObserved.current.parentElement.offsetWidth
+        setWidthParent(widthParentElement)
+      }
+    }
+
+    handleResize()
+
+    window.addEventListener("resize", handleResize)
+    window.addEventListener("scroll", handleScroll)
+
+    return () => {
+      window.removeEventListener("resize", handleResize)
+      window.removeEventListener("scroll", handleScroll)
+    }
+  }, [])
 
   useEffect(() => {
     if (nameSession) {
@@ -86,13 +111,21 @@ export const MultipleSeach = ({
   }, [nameSelectedOnPage, listPokemonsByType])
 
   return (
-    <StyleContainerMultipleSearch $inView={!inView} ref={refView}>
-      <InputSearch
-        valueByFather={valueByFather}
-        timeSearch={progressTime}
-        setNameSelectedOnPage={setNameSelectedOnPage}
-      />
-      <SelectFilterTypePokemon setListPokemonsByType={setListPokemonsByType} />
-    </StyleContainerMultipleSearch>
+    <>
+      <StyleContainerMultipleSearch
+        $widthParent={widthParent}
+        $isElementAboveViewport={isElementAboveViewport}
+      >
+        <div>
+          <InputSearch
+            valueByFather={valueByFather}
+            timeSearch={progressTime}
+            setNameSelectedOnPage={setNameSelectedOnPage}
+          />
+          <SelectFilterTypePokemon setListPokemonsByType={setListPokemonsByType} />
+        </div>
+      </StyleContainerMultipleSearch>
+      <span ref={elementObserved}></span>
+    </>
   )
 }
